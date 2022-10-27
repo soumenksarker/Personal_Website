8757376d6c4294fd90fb28bf626e277e42c7ee25
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2022-10-28 02:33:21 +0600

    Update 2_📚_Digital_CV.py

--- pages/2_📚_Digital_CV.py
+++ pages/2_📚_Digital_CV.py
@@ -29,7 +29,7 @@ PROJECTS = {
 "🏆 Bank Marketing Analysis Live Dashboard": "https://soumenksarker-live-bank-data-dashboard-app-2z8bky.streamlitapp.com/",
 "🏆 House Price Prediction from research to production environment":"https://fathomless-falls-91100.herokuapp.com/docs",
 "🏆 NLP literaure overview, A Sentiment Classification model by comparing naive Bayes, DNN, RNN, and LSTM classification models. Automatic Text Summarization with Transformer Architecture.": "https://github.com/soumenksarker/NLP-Specialization",
-"🏆 Built and Deployed an NLP application that reveals named entities, classifies sentiment, and does text summarization using spacy, textblob, genisma and T5": "https://soumenksarker-nlp-app-z47yvp.streamlitapp.com/",
+"🏆 Built and Deployed an NLP application that reveals named entities, classifies sentiment, and does text summarization using spacy, textblob, genism and T5": "https://soumenksarker-nlp-app-z47yvp.streamlitapp.com/",
 "🏆 Image Classification/Browser based model to classify Rock/Paper/Scissors Browser-based model, node.js, training with and without transfer learning, evaluating, alerting on browser!":"https://github.com/soumenksarker/TF-in-Deployment",
 "🏆 Built and deployed a CV application that detects faces, smiles, and eyes, do enhance, filters to add effect": "https://soumenksarker-face-detection-streamlit-app-bdy2rx.streamlitapp.com/",
 "🏆 Develop and Deploy a Personal Site with Chatbot Integration": "https://soumenksarker-personal-website-1--homepage-aqokf8.streamlitapp.com/",
