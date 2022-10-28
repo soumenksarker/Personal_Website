e32892d869041c87ff5d0b6e7e8019f28257a82a
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2022-10-28 12:54:28 +0600

    Update 3_✌️_Accomplishment.py

--- pages/3_✌️_Accomplishment.py
+++ pages/3_✌️_Accomplishment.py
@@ -121,10 +121,10 @@ if selected=="Live Chat":
                     st.session_state.history.append({"message": user_message, "is_user": True})
                     st.session_state.history.append({"message": message_bot, "is_user": False})
                #from copyreg import clear_extension_cache
-               for chat in st.session_state.history:
-                   st_message(**chat) 
+                for chat in st.session_state.history:
+                    st_message(**chat) 
                     # unpacking
-               st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
+                st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
             else:
                 st.warning("Incorrect Username/Password")
 
