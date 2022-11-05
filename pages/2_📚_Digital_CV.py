fbb22221c5d05c4b8ef5983ba048fac31191d263
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2022-11-05 19:22:51 +0600

    Update 2_📚_Digital_CV.py

--- pages/2_📚_Digital_CV.py
+++ pages/2_📚_Digital_CV.py
@@ -26,12 +26,12 @@ SOCIAL_MEDIA = {
     "Dataquest": "https://app.dataquest.io/profile/soumensarker.ice.iu.bd",
 }
 PROJECTS = {
-"🏆 Built and Deployed an NLP application that reveals named entities, classifies sentiment, and do OCR base Bangla and English text summarization using spacy, textblob, genism, T5 and tesseract": "https://huggingface.co/spaces/Soumen/Text-Summarization-and-NLP-tasks/",
-"🏆 Image captioning using huggingface API and streamlit ":"https://huggingface.co/spaces/Soumen/image_to_text",
-"🏆 Built and deployed an Image captioning streamlit application":"https://soumenksarker-imgcaptioning-app-izf09f.streamlitapp.com/",
-"🏆 Emotion detection!":"https://soumenksarker-emotion-recognition-app-770rmy.streamlitapp.com/",
-"🏆 Built and deployed a CV application that detects and classify objects, faces, smiles, and eyes, do enhance, filters to add effect.": "https://huggingface.co/spaces/Soumen/transform_image/",
-"🏆 Develop and Deploy a Personal Site with Chatbot Integration": "https://soumen-sarker-personal-site.streamlitapp.com/",
+"🏆 Built and deployed an NLP application that reveals named entities, classifies sentiment, and do OCR base Bangla and English text summarization using spacy, textblob, genism, T5 and tesseract.": "https://huggingface.co/spaces/Soumen/Text-Summarization-and-NLP-tasks/",
+"🏆 Image Captioning using huggingface API and streamlit.":"https://huggingface.co/spaces/Soumen/image_to_text",
+"🏆 Built and deployed an Image Captioning streamlit application.":"https://soumenksarker-imgcaptioning-app-izf09f.streamlitapp.com/",
+"🏆 Emotion detection application":"https://soumenksarker-emotion-recognition-app-770rmy.streamlitapp.com/",
+"🏆 Built and deployed a CV application that detects and classify objects, faces, smiles, and eyes, do enhance and filters to add effect.": "https://huggingface.co/spaces/Soumen/transform_image/",
+"🏆 Develop and deploy a Personal Websdite with Chatbot Integration": "https://soumen-sarker-personal-site.streamlitapp.com/",
 "🏆 NLP literaure overview, A Sentiment Classification model by comparing naive Bayes, DNN, RNN, and LSTM classification models. Automatic Text Summarization with Transformer Architecture.": "https://github.com/soumenksarker/NLP-Specialization",  
 "🏆 Image Classification/Browser based model to classify Rock/Paper/Scissors Browser-based model, node.js, training with and without transfer learning, evaluating, alerting on browser!":"https://github.com/soumenksarker/TF-in-Deployment",
 "🏆 Bank Marketing Analysis Live Dashboard": "https://soumenksarker-live-bank-data-dashboard-app-2z8bky.streamlitapp.com/",
