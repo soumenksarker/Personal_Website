d4c30da397c0231193f2890d77eb517e8f5aa8e4
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2022-11-15 19:28:31 +0600

    Update 2_📚_Digital_CV.py

--- pages/2_📚_Digital_CV.py
+++ pages/2_📚_Digital_CV.py
@@ -56,16 +56,16 @@ col1, col2 = st.columns(2, gap="small")
 with col1:
     st.image(profile_pic, width=230)
 
-with col2:
-    st.title(NAME)
-    st.write(DESCRIPTION)
-    st.download_button(
-        label=" 📄 Download Resume",
-        data=PDFbyte,
-        file_name=resume_file.name,
-        mime="application/octet-stream",
-    )
-    st.write("📫", EMAIL)
+# with col2:
+#     st.title(NAME)
+#     st.write(DESCRIPTION)
+#     st.download_button(
+#         label=" 📄 Download Resume",
+#         data=PDFbyte,
+#         file_name=resume_file.name,
+#         mime="application/octet-stream",
+#     )
+#     st.write("📫", EMAIL)
 
 
 # --- SOCIAL LINKS ---
