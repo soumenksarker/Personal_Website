eacacf1753a4c2bce568d8c288e7b1afe3f2109b
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2022-12-06 00:52:56 +0600

    Update 2_📚_Digital_CV.py

--- pages/2_📚_Digital_CV.py
+++ pages/2_📚_Digital_CV.py
@@ -7,7 +7,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-#resume_file = current_dir / "assets" / "CV.pdf"
+resume_file = current_dir / "assets" / "sks.pdf"
 profile_pic = current_dir / "assets" / "profile-pic (3).png"
 
 
@@ -46,8 +46,8 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 # --- LOAD CSS, PDF & PROFIL PIC ---
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-# with open(resume_file, "rb") as pdf_file:
-#     PDFbyte = pdf_file.read()
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
 profile_pic = Image.open(profile_pic)
 
 
@@ -56,16 +56,16 @@ col1, col2 = st.columns(2, gap="small")
 with col1:
     st.image(profile_pic, width=230)
 
-# with col2:
-#     st.title(NAME)
-#     st.write(DESCRIPTION)
-#     st.download_button(
-#         label=" 📄 Download Resume",
-#         data=PDFbyte,
-#         file_name=resume_file.name,
-#         mime="application/octet-stream",
-#     )
-#     st.write("📫", EMAIL)
+with col2:
+    st.title(NAME)
+    st.write(DESCRIPTION)
+    st.download_button(
+        label=" 📄 Download Resume",
+        data=PDFbyte,
+        file_name=resume_file.name,
+        mime="application/octet-stream",
+    )
+    st.write("📫", EMAIL)
 
 
 # --- SOCIAL LINKS ---
