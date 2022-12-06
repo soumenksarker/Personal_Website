fc8be24f352510561e4da558c69704c7ae4690a4
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2022-12-07 03:04:20 +0600

    Update 2_📚_Digital_CV.py

--- pages/2_📚_Digital_CV.py
+++ pages/2_📚_Digital_CV.py
@@ -7,7 +7,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "sks.pdf"
+resume_file = current_dir / "assets" / "CV.pdf"
 profile_pic = current_dir / "assets" / "profile-pic (3).png"
 
 
@@ -101,7 +101,6 @@ st.write(
 
 """
 )
-
 
 # --- Projects & Accomplishments ---
 st.write('\n')
