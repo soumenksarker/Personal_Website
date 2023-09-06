39b6fa5345ff161b560d9a560ec4f95adc2f8ea4
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-07 02:17:30 +0600

    Update PrivateBot.py

--- pages/PrivateBot.py
+++ pages/PrivateBot.py
@@ -50,21 +50,20 @@ def get_models():
 if "history" not in st.session_state:
     st.session_state.history = []
     st.title("Hello Chatbot")
-    def generate_answer():
-        tokenizer, model = get_models()
-        user_message = st.session_state.input_text
-        inputs = tokenizer(st.session_state.input_text, return_tensors="pt")
-        result = model.generate(**inputs)
-        message_bot = tokenizer.decode(
-            result[0], skip_special_tokens=True
-        )  # .replace("<s>", "").replace("</s>", "")
-        st.session_state.history.append({"message": user_message, "is_user": True})
-        st.session_state.history.append({"message": message_bot, "is_user": False})
-        from copyreg import clear_extension_cache
-        for chat in st.session_state.history:
-            st_message(**chat) 
-            # unpacking
-        st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
+    tokenizer, model = get_models()
+    user_message = st.session_state.input_text
+    inputs = tokenizer(st.session_state.input_text, return_tensors="pt")
+    result = model.generate(**inputs)
+    message_bot = tokenizer.decode(
+        result[0], skip_special_tokens=True
+    )  # .replace("<s>", "").replace("</s>", "")
+    st.session_state.history.append({"message": user_message, "is_user": True})
+    st.session_state.history.append({"message": message_bot, "is_user": False})
+    from copyreg import clear_extension_cache
+    for chat in st.session_state.history:
+        st_message(**chat) 
+        # unpacking
+    st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
 # else:
 #    st.warning("Incorrect Username/Password")
 else:
