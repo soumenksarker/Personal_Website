d0a521f414e7e514403aa5bd217ece5c628413ba
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-07 01:43:18 +0600

    Update Accomplishments.py

--- pages/Accomplishments.py
+++ pages/Accomplishments.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+from streamlit_extras.switch_page_button import switch_page
 # selected = option_menu(menu_title=None,
 # options= ["Certifications & Projects","Live Chat"], 
 # icons=['bookmarks fill', "chat-dots"],
@@ -60,4 +61,6 @@ elif option=="ML/DL courses & Projects":
     st.write("---")
     for cer, link in Certificates.items():
         st.write(f"[{cer}]({link})")
+if st.button("Home"):
+    switch_page("Homepage")
 
