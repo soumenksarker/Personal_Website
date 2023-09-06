e748b8ab71f9cc440bb28279eef0f64a3683e842
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-07 02:21:12 +0600

    Update PrivateBot.py

--- pages/PrivateBot.py
+++ pages/PrivateBot.py
@@ -1,4 +1,4 @@
-import sqlite3
+#import sqlite3
 #import torch
 import streamlit as st
 from streamlit_option_menu import option_menu
@@ -48,25 +48,25 @@ def get_models():
     return tokenizer, model
 if "history" not in st.session_state:
     st.session_state.history = []
-    st.title("Hello Chatbot")
-    tokenizer, model = get_models()
-    user_message = st.session_state.input_text
-    inputs = tokenizer(st.session_state.input_text, return_tensors="pt")
-    result = model.generate(**inputs)
-    message_bot = tokenizer.decode(
-        result[0], skip_special_tokens=True
-    )  # .replace("<s>", "").replace("</s>", "")
-    st.session_state.history.append({"message": user_message, "is_user": True})
-    st.session_state.history.append({"message": message_bot, "is_user": False})
-    from copyreg import clear_extension_cache
-    for chat in st.session_state.history:
-        st_message(**chat) 
-        # unpacking
-    st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
+st.title("Hello Chatbot")
+tokenizer, model = get_models()
+user_message = st.session_state.input_text
+inputs = tokenizer(st.session_state.input_text, return_tensors="pt")
+result = model.generate(**inputs)
+message_bot = tokenizer.decode(
+    result[0], skip_special_tokens=True
+)  # .replace("<s>", "").replace("</s>", "")
+st.session_state.history.append({"message": user_message, "is_user": True})
+st.session_state.history.append({"message": message_bot, "is_user": False})
+from copyreg import clear_extension_cache
+for chat in st.session_state.history:
+    st_message(**chat) 
+    # unpacking
+st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
 # else:
 #    st.warning("Incorrect Username/Password")
-else:
-   st.session_state.history = []
+# else:
+#    st.session_state.history = []
 
 # elif choice == "SignUp":
 #     st.subheader("Register Here!")
