170fe26899bb8f6bbb24143a67a964f5dd738235
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-07 17:38:01 +0600

    Update PrivateBot.py

--- pages/PrivateBot.py
+++ pages/PrivateBot.py
@@ -5,40 +5,10 @@ from streamlit_option_menu import option_menu
 from streamlit_chat import message as st_message
 from transformers import BlenderbotTokenizer
 from transformers import BlenderbotForConditionalGeneration
-# conn = sqlite3.connect('data.db')
-# c=conn.cursor()
-# def create_usertable():
-#     c.execute('CREATE TABLE IF NOT EXISTS usertable(username TEXT,password TEXT)')
-# def add_userdata(username, password):
-#     c.execute('INSERT INTO usertable(username, password) VALUES (?,?)',(username, password))
-#     conn.commit()
-# def login_user(username, password):
-#     c.execute('SELECT * FROM usertable WHERE username=? AND password=?', (username, password))
-#     data = c.fetchall()
-#     return data
-# def view_all_users():
-#     c.execute('SELECT * FROM usertable')
-#     data = c.fetchall()
-#     return data
-# st.subheader("SignUp to get access ...")
-# choice = option_menu(menu_title=None,
-# options= ["Login","SignUp"], 
-# icons=['Houses','book'],
-# menu_icon="cast",
-# default_index=0,
-# orientation="horizontal"
-# )
-st.title("Simple Chatbot for fun!")
-# if choice == "Login":
-#     #st.session_state.history=[]
-#     st.subheader("Login Section")
-#     username = st.text_input("User Name")
-#     password = st.text_input("Password", type='password')
-#     if st.checkbox("Login"):
-#         create_usertable()
-#         result=login_user(username, password)
-#         if result:
-#             st.success("Welcome, Logged In as {} .. feelng bored! Conversate ..".format(username))
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+    
 def get_models():
     # it may be necessary for other frameworks to cache the model
     # seems pytorch keeps an internal state of the conversation
@@ -46,8 +16,7 @@ def get_models():
     tokenizer = BlenderbotTokenizer.from_pretrained(model_name)
     model = BlenderbotForConditionalGeneration.from_pretrained(model_name)
     return tokenizer, model
-if "history" not in st.session_state:
-    st.session_state.history = []
+
 st.title("Hello Chatbot")
 tokenizer, model = get_models()
 user_message = st.session_state.input_text
@@ -63,17 +32,3 @@ for chat in st.session_state.history:
     st_message(**chat) 
     # unpacking
 st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
-# else:
-#    st.warning("Incorrect Username/Password")
-# else:
-#    st.session_state.history = []
-
-# elif choice == "SignUp":
-#     st.subheader("Register Here!")
-#     new_user = st.text_input("Username")
-#     new_password = st.text_input("Password", type='password')
-#     if st.button("SignUp"):
-#         create_usertable()
-#         add_userdata(new_user, new_password)
-#         st.success("You have successfully created a valid Account")
-#         st.info("Login to continue ...")
