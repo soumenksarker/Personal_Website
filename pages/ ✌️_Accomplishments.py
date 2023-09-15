1bd326d028d2cbae8421997a71c5e1303bf94123
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-15 13:23:55 +0600

    Update  ✌️_Accomplishments.py

--- pages/ ✌️_Accomplishments.py
+++ pages/ ✌️_Accomplishments.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 c1, c2=st.columns([2,3])
 if c1.button("Home"):
     switch_page("Homepage")
