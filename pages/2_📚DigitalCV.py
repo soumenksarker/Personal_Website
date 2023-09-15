4b7d813f63e19d1f961000a8a32db7504ab72af5
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-15 13:27:45 +0600

    Update 2_📚DigitalCV.py

--- pages/2_📚DigitalCV.py
+++ pages/2_📚DigitalCV.py
@@ -6,10 +6,6 @@ PAGE_TITLE = "Digital CV | SOUMEN SARKER"
 PAGE_ICON = ":wave:"
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 c1, c2=st.columns([2,3])
-if c1.button("Home"):
-    switch_page("Homepage")
-if c2.button("Accomplishments"):
-    switch_page("Accomplishments")
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
@@ -112,5 +108,8 @@ st.subheader("Projects")
 st.write("---")
 for project, link in PROJECTS.items():
     st.write(f"[{project}]({link})")
-if st.button("Home"):
-   switch_page("Homepage")
+if c1.button("Home"):
+    switch_page("Homepage")
+if c2.button("Accomplishments"):
+    switch_page("Accomplishments")
+
