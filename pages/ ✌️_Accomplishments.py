616633e63f427e5c482f5e5622c77d9b854a923b
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2023-09-15 13:26:21 +0600

    Update  ✌️_Accomplishments.py

--- pages/ ✌️_Accomplishments.py
+++ pages/ ✌️_Accomplishments.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
-st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+PAGE_TITLE = "Accomplishments| SOUMEN SARKER"
+st.set_page_config(page_title=PAGE_TITLE)
 c1, c2=st.columns([2,3])
 if c1.button("Home"):
     switch_page("Homepage")
