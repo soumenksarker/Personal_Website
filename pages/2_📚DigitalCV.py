329826e1ad41abf9b95ba36f19e29af179e411c6
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2024-05-03 23:35:05 +0600

    Update 2_📚DigitalCV.py

--- pages/2_📚DigitalCV.py
+++ pages/2_📚DigitalCV.py
@@ -25,8 +25,9 @@ SOCIAL_MEDIA = {
     "Codeforces": "https://codeforces.com/contests/with/Soumen_Sarkar",
 }
 PROJECTS = {
-"🏆 Write several services and microservices in Travelai's ETL pipeline by reading from api's, data extraction from json files, transform and load to store into postgres database's table. Tools/Tech: Cron, Python, SQLAlchemy ORM(INSERT, BULK UPDATE, e.t.c), OOP, RegEX, Postgres, S3": "",
+"🏆 Write several services and microservices in Travelai's ETL pipeline by reading from api's, data extraction from json files, transform and load to store into postgres database's table. Tools/Tech: Cron, Python, SQLAlchemy ORM(INSERT, BULK UPDATE, e.t.c), OOP, RegEX, Postgres, S3.": "",
 "🏆 Write SQL query in Apache Superset's SQL lab to grab analytics from connected database, execute as a cron to get several combinational time series csv chart data, mail into server using superset cron alert, write flask services to read data from mail server, match/analytics with api data and notify in dedicated team's channel.":"",
+"🏆 Write unit test for services in superset/web automation project written in flask":"",
 "🏆 Real-Time web scraping and visualization dashboard.": "https://github.com/soumenksarker/RealtimeScraperOpenSearchPostgresIntegration",
 "🏆 Real-time Q&A system to Analysis and Visualize on scraped data(integrated with first project).":"https://huggingface.co/spaces/Soumen/QA_to_AnalysisVisualize_on_your_data_and_database",
 "🏆 Real-time Credit Card fraud detection using Vertex AI, AutoML, BigQuery, Pub/Sub, Feature Store and Dataflow on GCP.":"https://github.com/soumenksarker/fraudfinder",
